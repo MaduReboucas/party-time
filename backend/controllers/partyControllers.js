3d39298a8eabd75a00f760c0004289052390e041
Duda Rebouças <mariadudareboucas@gmail.com>
2026-03-23 23:32:40 -0300

    Atualizando festas

--- backend/controllers/partyControllers.js
+++ backend/controllers/partyControllers.js
@@ -79,6 +79,35 @@ const partyController = {
       console.log(error);
     }
   },
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      const party = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        budget: req.body.budget,
+        image: req.body.image,
+        services: req.body.services,
+      };
+
+      if (party.services && !checkPartyBudget(party.budget, party.services)) {
+        res.status(406).json({ msg: "O seu orçamento é insuficiente." });
+        return;
+      }
+
+      const updateParty = await PartyModel.findByIdAndUpdate(is, party);
+
+      if (!updateParty) {
+        res.status(404).json({ msg: "Festa não encontrada." });
+      }
+
+      res.status(200).json({ party, msg: "Festa atualizada com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = partyController;
