5483c5e8932f4dcc9ca0631d531db0555f5f23c4
Duda Rebouças <mariadudareboucas@gmail.com>
2026-03-26 15:35:59 -0300

    Criando página de inserção de festas

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,5 +1,50 @@
 const CreateParty = () => {
-  return <div>CreateParty</div>;
+  return (
+    <div className="form-page">
+      <h2>Crie sua próxima festa</h2>
+      <p>Defina o seu o seu orçamento</p>
+      <form>
+        <label>
+          <span>Nome</span>
+          <input type="text" placeholder="Seja criativo..." required />
+        </label>
+        <label>
+          <span>Anfitrião</span>
+          <input type="text" placeholder="Quem está dando a festa?" required />
+        </label>
+        <label>
+          <span>Descrição</span>
+          <textarea
+            placeholder="Conte mais sobre a festa..."
+            required
+          ></textarea>
+        </label>
+        <label>
+          <span>Orçamento</span>
+          <input
+            type="text"
+            placeholder="Quanto você pretende insvestir?"
+            required
+          />
+        </label>
+        <label>
+          <span>Imagem:</span>
+          <input
+            type="text"
+            placeholder="Insira a URL de uma imagem"
+            required
+          />
+        </label>
+        <div>
+          <h2>Escolha os serviços</h2>
+          <div className="services-container">
+            <p>serviços...</p>
+          </div>
+        </div>
+        <input type="submit" value="Criar Festa" className="btn" />
+      </form>
+    </div>
+  );
 };
 
 export default CreateParty;
